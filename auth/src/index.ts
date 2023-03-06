9da2fe981052e2ab8936a1bb302294401c74be2a
kkulik112 <kkulik112@gmail.com>
2023-03-06 21:23:42 +0100

    Ingress configuration

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const port = 3000
 const app = express()
 app.use(express.json())
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Auth test')
+})
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
