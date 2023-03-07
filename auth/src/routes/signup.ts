5491c4bf4d65f49662c34156bcebe1a250748296
kkulik112 <kkulik112@gmail.com>
2023-03-07 20:37:52 +0100

    Added input validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,28 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
+import {body, validationResult} from 'express-validator'
 
 const router = express.Router()
 
-router.post('/api/users/signup', (req, res) => {
-  //const {email, password} = req.body
-  res.send('Sign Up')
+router.post('/api/users/signup', [
+  body('email')
+  .isEmail()
+  .withMessage('Invalid email address'),
+  body('password')
+  .trim()
+  .isLength({min: 4, max: 20})
+  .withMessage('Password must be between 4 and 20 characters long')
+],
+(req: Request, res: Response) => {
+  const errors = validationResult(req)
+  
+  if(!errors.isEmpty()){
+    return res.status(400).send(errors.array())
+  }
+  
+  const {email, password} = req.body
+
+  console.log('Creating a user...')
+  res.send({})
 })
 
 export {router as signupRouter}
