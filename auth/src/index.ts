b49e28fbaa2f02d124b2bcd9348cd65f4b811b0e
kkulik112 <kkulik112@gmail.com>
2023-03-11 21:02:07 +0100

    Added error handling middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+
+import { errorHandler } from './middlewares/error-handler'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
@@ -7,12 +9,13 @@ import { signupRouter } from './routes/signup'
 const port = 3000
 
 const app = express()
+
 app.use(express.json())
 app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
-
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
