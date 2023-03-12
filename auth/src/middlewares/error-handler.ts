b1e5f763666596be840631bb1a9b8b3aa6e47344
kkulik112 <kkulik112@gmail.com>
2023-03-12 19:45:48 +0100

    Structuralized custom errors and basic error handling

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,12 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express"
+
+import { CustomError } from "../errors/custom-error"
 
 export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log('Something went wrong', error)
+
+  if(error instanceof CustomError) {
+    return res.status(error.statusCode).send({errors: error.serializeErrors()})
+  }
 
   res.status(400).send({message: error.message})
 }
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,9 @@
 import express, {Request, Response} from 'express'
 import {body, validationResult} from 'express-validator'
 
+import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { RequestValidationError } from '../errors/request-validation-error'
+
 const router = express.Router()
 
 router.post('/api/users/signup', [
@@ -16,7 +19,7 @@ router.post('/api/users/signup', [
   const errors = validationResult(req)
   
   if(!errors.isEmpty()){
-    return res.status(400).send(errors.array())
+    throw new RequestValidationError(errors.array())
   }
   
   const {email, password} = req.body
