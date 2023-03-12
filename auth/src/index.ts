bdb2eff504b133984f45a427cd492b4d0de73707
kkulik112 <kkulik112@gmail.com>
2023-03-12 19:55:59 +0100

    Added route to handle 404 not found errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 
+import { NotFoundError } from './errors/not-found-error'
 import { errorHandler } from './middlewares/error-handler'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
@@ -15,6 +16,9 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
+
+app.get('*', () => {throw new NotFoundError()})
+
 app.use(errorHandler)
 
 app.listen(port, () => {
